e0156a328ce20e07943a4bc7efe96fb529f699ef
merugurajugoud <merugusindhuja2005@gmail.com>
2026-08-26 13:13:17 +0530

    added

---
+++ cses-problems/createstrings.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    vector<string>ans;
+    do
+    {
+        ans.push_back(s);
+    } while(next_permutation(s.begin(),s.end()));
+
+    cout<<ans.size()<<endl;
+    for(string x:ans)
+    {
+        cout<<x<<endl;
+    }
+    return 0;
+}
