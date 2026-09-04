7b1861d91cd8d4f3432d20e4eb6ef73fe513c626
merugurajugoud <merugusindhuja2005@gmail.com>
2026-09-04 18:22:45 +0530

    lc

---
+++ leetcode/3210.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string getEncryptedString(string s, int k)
+    {
+        k%=s.size();
+        reverse(s.begin(),s.begin()+k);
+        reverse(s.begin()+k,s.end());
+        reverse(s.begin(),s.end());
+        return s;
+    }
+};
