a51ba94ffb8383657d74cd30231b7104e586d187
merugurajugoud <merugusindhuja2005@gmail.com>
2026-09-21 18:28:46 +0530

    cc1

---
+++ codechef/FIZZBUZZ2305.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%2==0 || n==1)
+	        cout<<"Bob\n";
+	   else
+	    cout<<"Alice\n";
+	}
+
+}
