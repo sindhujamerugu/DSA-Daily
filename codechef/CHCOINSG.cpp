328ff0fdacf9cc67907b87fa3d104277e3a909ed
merugurajugoud <merugusindhuja2005@gmail.com>
2026-09-22 23:47:31 +0530

    cc

---
+++ codechef/CHCOINSG.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n%6==0)
+	    {
+	        cout<<"Misha"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"Chef"<<endl;
+	    }
+	}
+	return 0;
+
+}
