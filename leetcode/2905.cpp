de8d64d91d625cb92957f8fd6b2fb2d5143c5ad9
merugurajugoud <merugusindhuja2005@gmail.com>
2026-09-25 23:56:20 +0530

    lc

---
+++ leetcode/2905.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) 
+    {
+        int  n=nums.size();
+        int mn=0;
+        int mx=0;
+        for(int i=indexDifference;i<n;i++)
+        {
+            int j=i-indexDifference;
+            if(nums[j]<nums[mn])
+            {
+                mn=j;
+            }
+            if(nums[j]>nums[mx])
+            {
+                mx=j;
+            }
+            if(abs(nums[i]-nums[mn])>=valueDifference)
+            {
+                return {mn,i};
+            }
+            if(abs(nums[i]-nums[mx])>=valueDifference)
+            {
+                return {mx,i};
+            }
+        }
+    
+    return {-1,-1};
+        }
+};
